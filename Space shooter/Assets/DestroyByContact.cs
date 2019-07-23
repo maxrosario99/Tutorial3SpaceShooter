ba1d0e8d908875998d6365988065ced8e180794a
maxrosario99 <maxumum1000@gmail.com>
2019-07-22 23:21:54 -0400

    First commit

---
+++ Space shooter/Assets/DestroyByContact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    public AudioClip Asteroid;
+
+    public AudioSource musicSource;
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "boundary")
+        {
+            return;
+        }
+    Instantiate(explosion, transform.position, transform.rotation);
+          if (other.tag == "player") {
+              Instantiate(playerExplosion, transform.position, transform.rotation);}
+        Destroy(other.gameObject);
+        Destroy(gameObject); 
+         musicSource.clip = Asteroid;
+              musicSource.Play();
+    }
+}
---
+++ Space shooter/Assets/scripts/mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mover : MonoBehaviour
+{
+     public float speed;
+
+     private Rigidbody rb;
+
+     void Start()
+     {
+          rb = GetComponent<Rigidbody>();
+          rb.velocity = transform.forward * speed;
+     }
+}
